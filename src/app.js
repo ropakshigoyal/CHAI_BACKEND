b348b10d4518c9ef2c8c338bd4b70f0b8abdf7f9
ropakshi <ropakshigoyal@gmail.com>
2025-06-11 17:44:06 +0530

    utils done

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
 // app through express
 // db through mongoose
+
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+// always use use keyword for middleware config
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
